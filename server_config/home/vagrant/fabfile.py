c6bb002de77f042ba49f06c5eb94496907a8f768
Andrei Popovici <me@andreipopovici.co.uk>
2014-10-06 09:41:08 +0100

    Updated get_local_xml to use a predefined template
    - If local.xml.local is created in /vagrant that file is copied in the project
    - Or local.xml.template is copied in the project app/etc folder and $KEY &
    $DB_NAME get the key from the remote server + the host_url is set as a db name

--- server_config/home/vagrant/fabfile.py
+++ server_config/home/vagrant/fabfile.py
@@ -56,7 +56,19 @@ def git_clone():
 
 @task
 def get_local_xml():
-    local('cp /vagrant/local.xml.local /vagrant/htdocs/app/etc/local.xml')
+    """Copy local.xml.local or generate from  template and put the right values into it"""
+
+    if os.path.isfile('/vagrant/local.xml.local') == True:
+        local('cp /vagrant/local.xml.local %s'  % os.path.join(magento_root, 'app/etc/local.xml'))
+    else:
+        local('cp local.xml.template %s' % os.path.join(magento_root, 'app/etc/local.xml'))
+
+        with cd(config['magento_root']):
+            secret_key = run('/bin/grep "key" htdocs/app/etc/local.xml | sed \'s/.*\[//\' | sed \'s/\].*//\'')
+
+        with lcd(os.path.join(magento_root, 'app/etc')):
+            local('sed -i \'s/$KEY/%s/g\' local.xml' % secret_key)
+            local('sed -i \'s/$DB_NAME/%s/g\' local.xml' % config['magento_host_url'])
 
 @task
 def get_media_dump():
